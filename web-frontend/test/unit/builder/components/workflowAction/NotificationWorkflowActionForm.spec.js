3b64883aea5875d566c2c2ecc7981071607e7fcb
Jonathan Adeline <jonathan@baserow.io>
2026-09-01 13:31:29 +0400

    fix(builder): detect more misconfigurations

---
+++ web-frontend/test/unit/builder/components/workflowAction/NotificationWorkflowActionForm.spec.js
@@ -0,0 +1,35 @@
+import { defineComponent } from 'vue'
+import { mountSuspended } from '@nuxt/test-utils/runtime'
+
+import NotificationWorkflowActionForm from '@baserow/modules/builder/components/workflowAction/NotificationWorkflowActionForm.vue'
+
+const FormGroupStub = defineComponent({
+  name: 'FormGroup',
+  props: {
+    required: Boolean,
+  },
+  template: '<div><slot /></div>',
+})
+
+describe('NotificationWorkflowActionForm', () => {
+  test('does not mark either content field as individually required', async () => {
+    const wrapper = await mountSuspended(NotificationWorkflowActionForm, {
+      global: {
+        stubs: {
+          FormGroup: FormGroupStub,
+          InjectedFormulaInput: true,
+        },
+        mocks: {
+          $t: (key) => key,
+        },
+      },
+    })
+
+    expect(wrapper.findAllComponents(FormGroupStub)).toHaveLength(2)
+    expect(
+      wrapper
+        .findAllComponents(FormGroupStub)
+        .map((formGroup) => formGroup.props('required'))
+    ).toEqual([false, false])
+  })
+})
